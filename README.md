546e6cd21c610961b5670b0edc50712409181483
narayan jha <aadityamonu67@gmail.com>
2020-05-25 00:32:25 +0530

    contact us working

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # project-aayush
 Project Hosted On 20/5/2020
 Project hosting | SSL | Other end date : 21 August 2020
+
+Notification Service:
+sweetalert2 Popup
+
+Contact us form:
+Contact247 Service by wakeupcoders
---
+++ js/contact.js
@@ -0,0 +1,73 @@
+function send() {
+
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+
+    if (navigator.onLine) {
+      var formData = { To: "iotapp420@gmail.com", Name: name, Phone: phone, Email: email, Subject: subject, Message: message }
+      $('#cover-spin').show(30)
+
+      $.ajax({
+        url: "https://warm-refuge-56194.herokuapp.com/email",
+        type: "POST",
+        data: formData,
+        success: function (data, textStatus, jqXHR) {
+          //data - response from server
+          console.log(data.Status);
+          if (data.Status == '1') {
+            clearbox();
+            Swal.fire(
+              'Thanks for contacting us.',
+              'Your message has been sent. Soon! Someone from our team will get back to you.',
+              'success'
+            )
+
+          }
+          else {
+            clearbox();
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Something went wrong. Please try again later'
+            })
+
+          }
+          $('#cover-spin').hide(30);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          clearbox();
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong. Please try again later'
+          })
+          $('#cover-spin').hide(30);
+        }
+      });
+
+
+
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'You are not connected with internet.'
+
+      })
+      clearbox();
+    }
+
+  }
+
+
+  function clearbox() {
+    document.getElementById("name").value = '';
+    document.getElementById("phone").value = '';
+    document.getElementById("email").value = '';
+    document.getElementById("subject").value = '';
+    document.getElementById("message").value = '';
+
+  }
